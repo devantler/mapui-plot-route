58be54b99bcbdef81acec39dac2524579b7afd50
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2021-09-17 11:08:43 +0200

    Initial Commit

---
+++ MapPlotting/MapPlottingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPlottingApp.swift
+//  MapPlotting
+//
+//  Created by Nikolai Emil Damm on 17/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MapPlottingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
